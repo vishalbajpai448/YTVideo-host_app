8e1f853b6721d4c04e4ef741583ebccc6d0059c2
vishalbajpai448 <vishal.bajpai448@gmail.com>
2025-07-12 17:03:03 +0530

    Native Federation Config done for host mfe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+          loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+    },
+];
